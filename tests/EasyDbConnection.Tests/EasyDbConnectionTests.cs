ed980427a9492225f03cfda91439972ef7cf1190
Steve Atkinson <steve.atkinson@dootrix.com>
2019-09-10 15:57:22 +0100

    Formatted tests.

--- tests/EasyDbConnection.Tests/EasyDbConnectionTests.cs
+++ tests/EasyDbConnection.Tests/EasyDbConnectionTests.cs
@@ -29,7 +29,7 @@ namespace EasyDbConnection.Tests
         public void Open_opens_the_connection_when_it_is_not_open(ConnectionState state)
         {
             A.CallTo(() => _connection.State).Returns(state);
-            
+
             _easyDbConnection.Open();
 
             A.CallTo(() => _connection.Open())
@@ -46,13 +46,13 @@ namespace EasyDbConnection.Tests
         public void Open_does_nothing_when_the_connection_is_already_open(ConnectionState state)
         {
             A.CallTo(() => _connection.State).Returns(state);
-            
+
             _easyDbConnection.Open();
 
             A.CallTo(() => _connection.Open())
                 .MustNotHaveHappened();
         }
-        
+
         [Test]
         [TestCase(ConnectionState.Open)]
         [TestCase(ConnectionState.Broken)]
@@ -62,9 +62,9 @@ namespace EasyDbConnection.Tests
         public void Close_closes_the_connection_when_it_is_not_closed(ConnectionState state)
         {
             A.CallTo(() => _connection.State).Returns(state);
-            
+
             _easyDbConnection.Close();
-            
+
             A.CallTo(() => _connection.Close())
                 .MustHaveHappenedOnceExactly();
         }
@@ -73,9 +73,9 @@ namespace EasyDbConnection.Tests
         public void Close_does_nothing_when_the_connection_is_already_closed()
         {
             A.CallTo(() => _connection.State).Returns(ConnectionState.Closed);
-            
+
             _easyDbConnection.Close();
-            
+
             A.CallTo(() => _connection.Close())
                 .MustNotHaveHappened();
         }
@@ -85,7 +85,7 @@ namespace EasyDbConnection.Tests
         {
             A.CallTo(() => _command.ExecuteNonQuery())
                 .Returns(5);
-            
+
             var result = _easyDbConnection.ExecuteNonQuery("TEST", new[]
             {
                 new DbParam("@Test", DbType.String, "test")
@@ -108,14 +108,14 @@ namespace EasyDbConnection.Tests
             DbCommandExecutingEventArgs evt = null;
 
             _easyDbConnection.Executing += (o, e) => evt = e;
-            
+
             _easyDbConnection.ExecuteNonQuery(commandText, parameters);
 
             Assert.IsNotNull(evt);
             Assert.AreEqual(commandText, evt.CommandText);
             Assert.AreEqual(parameters, evt.Parameters);
         }
-        
+
         [Test]
         public void ExecuteNonQuery_invokes_Executed_event()
         {
@@ -127,7 +127,7 @@ namespace EasyDbConnection.Tests
             DbCommandExecutedEventArgs evt = null;
 
             _easyDbConnection.Executed += (o, e) => evt = e;
-            
+
             _easyDbConnection.ExecuteNonQuery(commandText, parameters);
 
             Assert.IsNotNull(evt);
@@ -141,7 +141,7 @@ namespace EasyDbConnection.Tests
         {
             A.CallTo(() => _command.ExecuteScalar())
                 .Returns("abc123");
-            
+
             var result = _easyDbConnection.ExecuteScalar("TEST", new[]
             {
                 new DbParam("@Test", DbType.String, "test")
@@ -152,7 +152,7 @@ namespace EasyDbConnection.Tests
             A.CallTo(() => _command.ExecuteScalar())
                 .MustHaveHappenedOnceExactly();
         }
-        
+
         [Test]
         public void ExecuteScalar_invokes_Executing_event()
         {
@@ -164,14 +164,14 @@ namespace EasyDbConnection.Tests
             DbCommandExecutingEventArgs evt = null;
 
             _easyDbConnection.Executing += (o, e) => evt = e;
-            
+
             _easyDbConnection.ExecuteScalar(commandText, parameters);
 
             Assert.IsNotNull(evt);
             Assert.AreEqual(commandText, evt.CommandText);
             Assert.AreEqual(parameters, evt.Parameters);
         }
-        
+
         [Test]
         public void ExecuteScalar_invokes_Executed_event()
         {
@@ -183,7 +183,7 @@ namespace EasyDbConnection.Tests
             DbCommandExecutedEventArgs evt = null;
 
             _easyDbConnection.Executed += (o, e) => evt = e;
-            
+
             _easyDbConnection.ExecuteScalar(commandText, parameters);
 
             Assert.IsNotNull(evt);
@@ -191,15 +191,15 @@ namespace EasyDbConnection.Tests
             Assert.AreEqual(parameters, evt.Parameters);
             Assert.IsNotNull(evt.TimeTaken);
         }
-        
+
         [Test]
         public void ExecuteReader_runs_ExecuteReader_on_the_IDbCommand()
         {
             var dataReader = A.Fake<IDataReader>();
-            
+
             A.CallTo(() => _command.ExecuteReader())
                 .Returns(dataReader);
-            
+
             var result = _easyDbConnection.ExecuteReader("TEST", new[]
             {
                 new DbParam("@Test", DbType.String, "test")
@@ -210,7 +210,7 @@ namespace EasyDbConnection.Tests
             A.CallTo(() => _command.ExecuteReader())
                 .MustHaveHappenedOnceExactly();
         }
-        
+
         [Test]
         public void ExecuteReader_invokes_Executing_event()
         {
@@ -222,14 +222,14 @@ namespace EasyDbConnection.Tests
             DbCommandExecutingEventArgs evt = null;
 
             _easyDbConnection.Executing += (o, e) => evt = e;
-            
+
             _easyDbConnection.ExecuteReader(commandText, parameters);
 
             Assert.IsNotNull(evt);
             Assert.AreEqual(commandText, evt.CommandText);
             Assert.AreEqual(parameters, evt.Parameters);
         }
-        
+
         [Test]
         public void ExecuteReader_invokes_Executed_event()
         {
@@ -241,7 +241,7 @@ namespace EasyDbConnection.Tests
             DbCommandExecutedEventArgs evt = null;
 
             _easyDbConnection.Executed += (o, e) => evt = e;
-            
+
             _easyDbConnection.ExecuteReader(commandText, parameters);
 
             Assert.IsNotNull(evt);
