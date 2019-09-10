df0dba8865248b6101e7492f498335beddff0926
Steve Atkinson <steve.atkinson@dootrix.com>
2019-09-10 16:26:05 +0100

    Moved the properties.

--- src/EasyDbConnection/Events/DbCommandExecutedEventArgs.cs
+++ src/EasyDbConnection/Events/DbCommandExecutedEventArgs.cs
@@ -4,15 +4,15 @@ namespace EasyDbConnection.Events
 {
     public class DbCommandExecutedEventArgs : EventArgs
     {
-        public string CommandText { get; }
-        public DbParam[] Parameters { get; }
-        public TimeSpan TimeTaken { get; }
-
         public DbCommandExecutedEventArgs(string commandText, DbParam[] parameters, TimeSpan timeTaken)
         {
             CommandText = commandText;
             Parameters = parameters;
             TimeTaken = timeTaken;
         }
+
+        public string CommandText { get; }
+        public DbParam[] Parameters { get; }
+        public TimeSpan TimeTaken { get; }
     }
 }
--- src/EasyDbConnection/Events/DbCommandExecutingEventArgs.cs
+++ src/EasyDbConnection/Events/DbCommandExecutingEventArgs.cs
@@ -4,13 +4,13 @@ namespace EasyDbConnection.Events
 {
     public class DbCommandExecutingEventArgs : EventArgs
     {
-        public string CommandText { get; }
-        public DbParam[] Parameters { get; }
-
         public DbCommandExecutingEventArgs(string commandText, DbParam[] parameters)
         {
             CommandText = commandText;
             Parameters = parameters;
         }
+
+        public string CommandText { get; }
+        public DbParam[] Parameters { get; }
     }
 }
